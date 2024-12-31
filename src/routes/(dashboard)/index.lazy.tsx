d489c2e9afb5792876c555ddc2743e6e4c569d0a
Miled Ameur <midou.slm8@gmail.com>
2025-01-01 00:48:51 +0100

    adding the sidebar

--- src/routes/(dashboard)/index.lazy.tsx
+++ src/routes/(dashboard)/index.lazy.tsx
@@ -1,18 +1,89 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { Euro, ReceiptEuro, ReceiptText, Users } from "lucide-react";
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
 
-export const Route = createLazyFileRoute('/(dashboard)/')({
+export const Route = createLazyFileRoute("/(dashboard)/")({
   component: RouteComponent,
-})
+});
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+} satisfies ChartConfig;
+
+const cardsData = [
+  {
+    title: "Total Client",
+    value: 65,
+    icon: Users,
+  },
+  {
+    title: "Total Devis",
+    value: 517,
+    icon: ReceiptText,
+  },
+  {
+    title: "Total Facture",
+    value: 65,
+    icon: ReceiptEuro,
+  },
+  {
+    title: "Chiffre d'affaire",
+    value: 65,
+    icon: Euro,
+  },
+];
 
 export default function RouteComponent() {
   return (
     <div className="flex flex-1 flex-col gap-4 p-4">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
-        <div className="aspect-video rounded-xl bg-muted/50" />
+      <div className="grid auto-rows-min gap-4 md:grid-cols-4">
+        {cardsData.map((card) => (
+          <Card key={card.title}>
+            <CardHeader>
+              <CardTitle>{card.title}</CardTitle>
+              <CardContent className="flex flex-row items-center justify-between">
+                <b className="text-2xl ">{card.value}</b>
+                <card.icon width={40} height={40} />
+              </CardContent>
+            </CardHeader>
+          </Card>
+        ))}
       </div>
-      <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      <ChartContainer
+        config={chartConfig}
+        className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min"
+      >
+        <BarChart accessibilityLayer data={chartData}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="month"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+            tickFormatter={(value) => value.slice(0, 3)}
+          />
+          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+        </BarChart>
+      </ChartContainer>
     </div>
-  )
+  );
 }
--- src/routes/-components/app-sidebar.tsx
+++ src/routes/-components/app-sidebar.tsx
@@ -1,10 +1,11 @@
-import * as React from "react"
+import * as React from "react";
 
-import { SearchForm } from "./search-form"
-import { VersionSwitcher } from "./version-switcher"
+import { SearchForm } from "./search-form";
+import { VersionSwitcher } from "./version-switcher";
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -13,139 +14,52 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarRail,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
+import { Calendar, Home, ReceiptEuro, ReceiptText, Settings } from "lucide-react";
+import { NavUser } from "./nav-user";
 
 // This is sample data.
 const data = {
   versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
   navMain: [
     {
-      title: "Getting Started",
+      title: "General",
       url: "#",
       items: [
         {
-          title: "Installation",
-          url: "#",
+          title: "accueil",
+          url: "/",
+          icon: Home,
         },
         {
-          title: "Project Structure",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Building Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Routing",
-          url: "#",
+          title: "Devis",
+          url: "/devis",
+          icon: ReceiptText,
         },
         {
-          title: "Data Fetching",
-          url: "#",
-          isActive: true,
+          title: "Facture",
+          url: "/facture",
+          icon: ReceiptEuro,
         },
         {
-          title: "Rendering",
+          title: "Agenda",
           url: "#",
+          icon: Calendar,
         },
         {
-          title: "Caching",
-          url: "#",
-        },
-        {
-          title: "Styling",
-          url: "#",
-        },
-        {
-          title: "Optimizing",
-          url: "#",
-        },
-        {
-          title: "Configuring",
-          url: "#",
-        },
-        {
-          title: "Testing",
-          url: "#",
-        },
-        {
-          title: "Authentication",
-          url: "#",
-        },
-        {
-          title: "Deploying",
-          url: "#",
-        },
-        {
-          title: "Upgrading",
-          url: "#",
-        },
-        {
-          title: "Examples",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "API Reference",
-      url: "#",
-      items: [
-        {
-          title: "Components",
-          url: "#",
-        },
-        {
-          title: "File Conventions",
-          url: "#",
-        },
-        {
-          title: "Functions",
-          url: "#",
-        },
-        {
-          title: "next.config.js Options",
-          url: "#",
-        },
-        {
-          title: "CLI",
-          url: "#",
-        },
-        {
-          title: "Edge Runtime",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Architecture",
-      url: "#",
-      items: [
-        {
-          title: "Accessibility",
-          url: "#",
-        },
-        {
-          title: "Fast Refresh",
-          url: "#",
-        },
-        {
-          title: "Next.js Compiler",
-          url: "#",
-        },
-        {
-          title: "Supported Browsers",
-          url: "#",
-        },
-        {
-          title: "Turbopack",
-          url: "#",
+          title: "Paramètres",
+          url: "/settings",
+          icon: Settings,
         },
       ],
     },
   ],
-}
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -166,8 +80,11 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               <SidebarMenu>
                 {item.items.map((item) => (
                   <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild isActive={item.isActive}>
-                      <a href={item.url}>{item.title}</a>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </a>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
                 ))}
@@ -176,7 +93,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
           </SidebarGroup>
         ))}
       </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
